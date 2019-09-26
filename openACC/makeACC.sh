01e875f50cd2066beac7ea8850136a9f913e205f
dxadam <adamgrusky@msn.com>
2019-09-26 02:01:24 -0400

    fixed compile options for multicore

--- openACC/makeACC.sh
+++
@@ -1,3 +0,0 @@
-pgcc -acc -Minfo -ta=tesla:cuda9.1 -fast  poissonACC.c -o poissonACC
-
-# add option -O3 for compiler optimization
---
+++ openACC/makeACC_cpu.sh
@@ -0,0 +1,3 @@
+pgcc -acc -Minfo -ta=multicore poissonACC.c vtk.c -lm -o poissonACC_cpu
+
+# add option -O3 for compiler optimization
